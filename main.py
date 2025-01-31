e5e5f5772fdc796a0ff081fb90536d01c861cf96
Colin <kappa6708@gmail.com>
2025-01-31 07:49:13 -0600

    Simple weather app. Fun beginner project with Flask and Open-Meteo API.

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+cache_session = requests_cache.CachedSession(".cache', expires_after = 3600")
+retry_session = retry(cache_session, retries = 5, backoff_factor =0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+def get_weather(city):
+
+    city_dict = {
+        "name": "Chicago" "Tokyo",
+        "lat": "42" "-88",
+        "long": "36" "140",
+    }
+
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    lat = 0
+    long = 0
+
+    for i in city_dict["name"]:
+        if i == city:
+            lat = city_dict["lat"]
+            long = city_dict["long"]
+
+    params = {
+        f"latitude": lat,
+	    f"longitude": long,
+	    f"hourly": "temperature_2m"
+    }
+
+    responses = openmeteo.weather_api(url, params=params)
+
+    return responses
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/Chicago')
+def chi():
+    weather = get_weather("Chicago")
+    return render_template('city.html', city = "Chicago")
+
+@app.route('/Tokyo')
+def tokyo():
+    weather = get_weather("Tokyo")
+    return render_template('city.html', city = "Tokyo")
+
+if __name__ == '__main__':
+    app.run(debug=True)
